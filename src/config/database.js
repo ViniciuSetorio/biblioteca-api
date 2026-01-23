9c186f38ee035ef3a471ee46080c7a3c759ab914
Vinicius Alves <viniciusasetorio@protonmail.com>
2026-01-22 22:03:42 -0300

    Configuração do banco de dados local

---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+pool.on("connect", () => {
+  console.log("Conectado ao banco de dados Postgres.");
+});
+
+pool.on("error", (err) => {
+  console.log("Erro ao conectar ao banco de dados Postgres: ", err);
+});
+
+export default pool;
