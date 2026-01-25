1fac9ed9db02fd1a98376fad5ba5f3ba7978c7f9
Vinicius Alves <viniciusasetorio@protonmail.com>
2026-01-25 16:49:50 -0300

    Correção no export pool

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
 import { Pool } from "pg";
 
-export const pool = new Pool({
+const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
 });
+
+export default pool;
