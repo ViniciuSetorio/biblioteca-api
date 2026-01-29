8318a537de09466558d40a9e994904c5ca4af93f
Vinicius Alves <viniciusasetorio@protonmail.com>
2026-01-28 23:56:52 -0300

    Refatorando a conexão com database para padrão singleton mais claro

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,21 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-});
+let poolInstance;
 
-export default pool;
+function createPool() {
+  return new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+  });
+}
+
+export default function getDatabase() {
+  if (!poolInstance) {
+    poolInstance = createPool();
+  }
+
+  return poolInstance;
+}
